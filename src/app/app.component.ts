f71ecf99e835c4fdfa2aada1c82b38985f48a2d5
Pierre Beaufaron <p.beaufaron@gmail.com>
2024-10-18 13:26:55 +0200

    Commit initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./other/navbar/navbar.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+
+  // VARIABLES GLOBALES
+  APP_ENV: string = "DEV"; // PROD or DEV
+  API_URL: string = "";
+
+  ngOnInit(): void {
+
+    // Gestion de l'API
+    if (this.APP_ENV == "DEV") {
+      this.API_URL = "http://localhost:8080/api";
+    } else if (this.APP_ENV == "PROD") {
+      this.API_URL = "http://localhost:8080/api";
+    } else {
+      this.API_URL = "http://localhost:8080/api";
+    }
+
+  }
+
+  // FONCTONS GLOBALES
+
+}
